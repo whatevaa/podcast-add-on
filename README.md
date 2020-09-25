a2946f20ebcd5c08ef3178e2cda572efb5c4c172
whatevaa <eva.blot.eb@gmail.com>
2020-09-25 13:50:00 +0200

    get and download mp3, display download button and icon extension

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # podcast-add-on
 
-Extension web de recherche et de téléchargement de podcast France culture (et Radio France)
+Extension web de téléchargement de podcasts France culture, qui ne sont plus disponibles sur le flux RSS du site et donc non téléchargeables sans passer par l'appli Radio France
---
+++ downloadPodcast.js
@@ -0,0 +1,43 @@
+
+
+const playButtons = document.getElementsByClassName("replay-button paused aod")
+
+// Get my image to the appropriate format
+// const myImage = browser.runtime.getURL("icons/Cloud-Download-256.png");
+const myImage = browser.extension.getURL("icons/Cloud-Download-256.png");
+
+// For each button in the array playButtons, use getAndDisplayDlLink function
+for (let pB of playButtons) {
+    getAndDisplayDlLink(pB);
+}
+
+// Function to (1) get the link for mp3 and (2) display a button to download
+function getAndDisplayDlLink(pB) {
+    const link = getDownloadLink(pB);
+    displayDownloadLink(pB, link);
+}
+
+// Find the link to mp3 ressource 
+function getDownloadLink(pB) {
+    return pB.getAttribute("data-asset-source")
+}
+
+
+/**
+ * 
+ * @param pB : button element
+ * @param link : download link
+ */
+function displayDownloadLink(pB, link) {
+    // 1. In JS, create HTML <a href= link download> + add img + add class for css
+    const text = "download" 
+    var a = document.createElement('a');
+    a.href = link;
+    a.setAttribute('download',link);
+    a.setAttribute('target',"_blank");
+    a.innerHTML = text;
+    a.innerHTML = `<img src=\"${myImage}\"/>`;  
+    pB.parentElement.appendChild(a);
+
+    a.classList.add("addButton"); 
+}
