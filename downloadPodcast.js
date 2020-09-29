9c01b6597008334d5089ceafdf9bdba37452c35a
whatevaa <eva.blot.eb@gmail.com>
2020-09-29 18:23:54 +0200

    Check url every 200ms and reload extension if needed

--- downloadPodcast.js
+++ downloadPodcast.js
@@ -1,43 +1,59 @@
+function activateExtension() {
+    console.log("activateee");
+    const playButtons = document.getElementsByClassName("replay-button paused aod")
 
-
-const playButtons = document.getElementsByClassName("replay-button paused aod")
-
-// Get the ULR of the resource (the image on web_accessible_resources of manifest.json) in the format "moz-extension://<extension-UUID>/images/my-image.png"
-// const myImage = browser.runtime.getURL("icons/Cloud-Download-256.png");
-const myImage = browser.extension.getURL("icons/Cloud-Download-256.png");
-
-// For each button in the array playButtons, use getAndDisplayDlLink function
-for (let pB of playButtons) {
-    getAndDisplayDlLink(pB);
+    // Get the ULR of the resource (the image on web_accessible_resources of manifest.json) in the format "moz-extension://<extension-UUID>/images/my-image.png"
+    // const myImage = browser.runtime.getURL("icons/Cloud-Download-256.png");
+    const myImage = browser.extension.getURL("icons/Cloud-Download-256.png");
+    
+    // For each button in the array playButtons, use getAndDisplayDlLink function
+    for (let pB of playButtons) {
+        getAndDisplayDlLink(pB);
+    }
+    
+    // Function to (1) get the link for mp3 and (2) display a button to download
+    function getAndDisplayDlLink(pB) {
+        const link = getDownloadLink(pB);
+        displayDownloadLink(pB, link);
+    }
+    
+    // Find the link to mp3 ressource 
+    function getDownloadLink(pB) {
+        return pB.getAttribute("data-asset-source")
+    }
+    
+    
+    /**
+     * 
+     * @param pB : button element
+     * @param link : download link
+     */
+    function displayDownloadLink(pB, link) {
+        // 1. In JS, create HTML <a href= link download> + add img + add class for css
+        const text = "download" 
+        var a = document.createElement('a');
+        a.href = link;
+        a.setAttribute('download',link);
+        a.setAttribute('target',"_blank");
+        a.innerHTML = text;
+        a.innerHTML = `<img src=\"${myImage}\"/>`;  
+        pB.parentElement.appendChild(a);
+    
+        a.classList.add("addButton"); 
+    }
 }
 
-// Function to (1) get the link for mp3 and (2) display a button to download
-function getAndDisplayDlLink(pB) {
-    const link = getDownloadLink(pB);
-    displayDownloadLink(pB, link);
-}
+// Keep last url to compare with new one
+let windowLocation = window.location.href;
+// First load
+activateExtension();
 
-// Find the link to mp3 ressource 
-function getDownloadLink(pB) {
-    return pB.getAttribute("data-asset-source")
-}
+// Check url every 200ms and reload extension if needed 
+window.setInterval(() => {
+    // If url change realod extension and update windowLocation var.
+    if (windowLocation != window.location.href) {
+        windowLocation = window.location.href;
 
-
-/**
- * 
- * @param pB : button element
- * @param link : download link
- */
-function displayDownloadLink(pB, link) {
-    // 1. In JS, create HTML <a href= link download> + add img + add class for css
-    const text = "download" 
-    var a = document.createElement('a');
-    a.href = link;
-    a.setAttribute('download',link);
-    a.setAttribute('target',"_blank");
-    a.innerHTML = text;
-    a.innerHTML = `<img src=\"${myImage}\"/>`;  
-    pB.parentElement.appendChild(a);
-
-    a.classList.add("addButton"); 
-}
+        activateExtension();
+    }
+}, 200)
